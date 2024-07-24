bc7fe680cbd261dc87ee343c43ad5a98ee67a56d
Rosihan Ardiansyah <rosihan17@gmail.com>
2024-07-24 11:01:07 +0800

    placeholder image

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,43 +11,43 @@ class HomeController extends Controller
     {
         $products = [
             [
-                'image' => 'assets/img/coockies.jpg',
+                'image' => 'assets/img/placeholder.jpg',
                 'title' => 'Cookies',
                 'description' => 'Crispy texture because it is baked at high temperature plus chocochips to make the taste sweet.',
                 'price' => 'IDR 30.000'
             ],
             [
-                'image' => 'assets/img/croissant.jpg',
+                'image' => 'assets/img/placeholder.jpg',
                 'title' => 'Croissant',
                 'description' => 'Crispy texture on the outside and soft on the inside, filled with butter which makes it taste delicious, perfect for breakfast',
                 'price' => 'IDR 20.000'
             ],
             [
-                'image' => 'assets/img/baguette.jpg',
+                'image' => 'assets/img/placeholder.jpg',
                 'title' => 'Baguette',
                 'description' => 'Bread that is elongated in shape and has a hard skin texture but is soft inside, elongated like a stick, usually served sliced and spread with butter and garlic.',
                 'price' => 'IDR 40.000'
             ],
             [
-                'image' => 'assets/img/pretzels.jpg',
+                'image' => 'assets/img/placeholder.jpg',
                 'title' => 'Pretzel',
                 'description' => 'The taste of pretzels is generally savory or slightly sweet. The texture of the pretzels is usually soft and tender. Generally, pretzels are served with a sprinkling of coarse salt.',
                 'price' => 'IDR 25.000'
             ],
             [
-                'image' => 'assets/img/sourdough.jpg',
+                'image' => 'assets/img/placeholder.jpg',
                 'title' => 'Sourdough',
                 'description' => 'This easy-to-digest bread goes well with butter and garlic, the texture is dense and fibrous and the taste is slightly sour.',
                 'price' => 'IDR 50.000'
             ],
             [
-                'image' => 'assets/img/donuts.jpg',
+                'image' => 'assets/img/placeholder.jpg',
                 'title' => 'Donuts',
                 'description' => 'Soft Bread Texture Covered with sweet Chocolate and also sprinkled with nuts and has other flavors.',
                 'price' => 'IDR 20.000'
             ],
             [
-                'image' => 'assets/img/muffin.jpg',
+                'image' => 'assets/img/placeholder.jpg',
                 'title' => 'Muffin',
                 'description' => 'The texture is soft with vanilla cream filling on top and also sprinkled with cocoa powder and choco chips.',
                 'price' => 'IDR 20.000'
